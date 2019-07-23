00d37b6479c6020fb23b6eda2defaaf2f05458c7
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-22 22:57:51 -0400

    Added AddMovie component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './App.css';
 import Nav from './Nav';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 import {MovieProvider} from './MovieContext';
 
 
 
 
 function App() {
-
- 
   return (
     <MovieProvider>
       <div className="App">
       <Nav />
+      <AddMovie />
       <MovieList />
       </div>
      </MovieProvider>
